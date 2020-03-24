58aabbf6350b96c3c527a147b3103705ccf69ae6
AVAE Jimmy (EXT) <JAVAE@intranet.opt>
2020-03-24 13:50:56 +1100

    fix(APIKEY)

--- lib/EmploiService.dart
+++ lib/EmploiService.dart
@@ -3,13 +3,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import 'Emploi.dart';
+import 'app_config.dart';
 
 const Map<String, String> HEADERS = {
-  'x-rapidapi-key': '9fde4c46a9mshe439586e7fd2280p17ab6ejsnbe1af0bb4192',
+  'x-rapidapi-key': apiKey,
   'Content-type': 'application/json; charset=utf-8'
 };
 
 class EmploiService {
+
   static Future<List<Emploi>> getLatestEmplois() async{
 
     var response= await http.get('https://emploi-nouvelle-caledonie.p.rapidapi.com/emploi/latest', headers : HEADERS );
---
+++ lib/app_config.dart
@@ -0,0 +1,10 @@
+// lib/app_config.dart
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+const apiKey = "660ac6c35emsh972da53a6ee26b1p1afb5cjsnb8006161e8af";
+
+class AppConfig {
+}
