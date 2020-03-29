6a9dea7655523fd101a817365cb92594b9b8a967
AVAE Jimmy (EXT) <JAVAE@intranet.opt>
2020-03-30 09:43:51 +1100

    fix(change Id App)

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -2,9 +2,10 @@ import 'package:EmploiNC/Emploi.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:shimmer/shimmer.dart';
 
 import 'EmploiService.dart';
-import 'SplashScreen.dart';
+
 
 class MyApp extends StatelessWidget {
 
@@ -20,10 +21,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.blue[900],
         brightness: Brightness.dark,
       ),
-      home: new SplashScreen(),
-      routes: <String, WidgetBuilder>{
-        '/HomeScreen': (BuildContext context) => new Dashboard()
-      },
+      home:  Dashboard(),
     );
   }
 }
@@ -66,88 +64,100 @@ class _Dashboard extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-        appBar: AppBar(
-          title: Text(_title),
-        ),
-        body: FutureBuilder<List<Emploi>>(
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      body: FutureBuilder<List<Emploi>>(
           future: emplois,
           builder: (BuildContext context, AsyncSnapshot<List<Emploi>> snapshot) {
             if (!snapshot.hasData || snapshot.connectionState == ConnectionState.waiting)
-              return Center(child: CircularProgressIndicator());
-
               return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder:(BuildContext context, int index) {
-                    return Card(
+                  itemCount: 10,
+                  // Important code
+
+                  itemBuilder: (context, index) => Shimmer.fromColors(
+                      child: Card (
+                          child: InkWell(
+                            child: Row(
+                              children: <Widget>[
+                                Container(
+                                  height: 100.0,
+                                ),
+                              ],
+                            ),
+                          )
+                      ),
+                    baseColor: Colors.black12,
+                  highlightColor: Colors.black26,
+                  ));
+
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder:(BuildContext context, int index) {
+                  return Card(
                       elevation: 3,
-                        child: InkWell(
+                      child: InkWell(
                           onTap: () => launch(snapshot.data[index].url),
                           child: Row(
                             children: <Widget>[
-                            Container(
-                              height: 100.0,
-                              width: 80.0,
-                              padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
-                                child: new Image.memory(snapshot.data[index].decodeLogo(),fit: BoxFit.contain  ),
-                            ),
-                            Container(
-                              height: 100,
-                              child: Padding(
+                              Container(
+                                height: 100.0,
+                                width: 80.0,
                                 padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
-                                    child: Container(
-                                      width: 260,
-                                      child: Text(snapshot.data[index].titreOffre,style:
-                                        TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 15,
-                                        ), overflow: TextOverflow.ellipsis
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                      ),
-                                      child: Text(snapshot.data[index].typeContrat,textAlign: TextAlign.center,),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                                    child: Container(
-                                      child: Row(
-                                        children: <Widget>[
-                                          Padding(
-                                            padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                                              child: Text(snapshot.data[index].communeEmploi),
-                                            ),
-                                          Padding(
-                                            padding: EdgeInsets.fromLTRB(100, 3, 0, 3),
-                                            child: Text(snapshot.data[index].aPourvoirLe),
-                                          ),
-                                      ]
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                                child: new Image.memory(snapshot.data[index].decodeLogo(),fit: BoxFit.contain  ),
                               ),
-                            ),
+                              Container(
+                                height: 100,
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                                        child: Container(
+                                          width: 260,
+                                          child: Text(snapshot.data[index].titreOffre,style:
+                                          TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15,
+                                          ), overflow: TextOverflow.ellipsis
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                          ),
+                                          child: Text(snapshot.data[index].typeContrat,textAlign: TextAlign.center,),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                                        child: Container(
+                                          width: 260,
+                                          child: Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: <Widget>[
+                                                  new Text(snapshot.data[index].communeEmploi),
+                                                  new Text(snapshot.data[index].aPourvoirLe)
+                                                ]
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
                           )
-                        ],
-                        )
-                    )
+                      )
                   );
                 }
-              );
+            );
           }
-        ),
+      ),
     );
   }
 }
-
-
