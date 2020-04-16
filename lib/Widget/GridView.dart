8335dbddf0c1c445fe45f3e1a01b92aa8f3e175b
AVAE Jimmy (EXT) <JAVAE@intranet.opt>
2020-04-16 11:50:42 +1100

    fix(GridHeight)

--- lib/Widget/GridView.dart
+++ lib/Widget/GridView.dart
@@ -59,82 +59,84 @@ class _GridViewWidget extends State<GridViewWidget> {
 
   Widget buildList(AsyncSnapshot<List<Emploi>> snapshot) {
 
-    double  screenWidth  = MediaQuery.of(context).size.width;
-    double _crossAxisSpacing = 0, _mainAxisSpacing = 0, _aspectRatio = 0.55;
     int _crossAxisCount = 2;
-    var width = (screenWidth - ((_crossAxisCount - 1) * _crossAxisSpacing)) / _crossAxisCount;
-    var height = width / _aspectRatio ;
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
 
     return GridView.builder(
         itemCount: snapshot.data.length,
         gridDelegate:
         new SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: _crossAxisCount,
-          crossAxisSpacing: _crossAxisSpacing,
-          mainAxisSpacing: _mainAxisSpacing,
-          childAspectRatio: _aspectRatio
+          childAspectRatio: (itemWidth / itemHeight),
         ),
         itemBuilder: (BuildContext context, int index) {
           return  ListView(
             padding: EdgeInsets.all(8),
             children: <Widget>[
-              Padding(
+              Container(
+
                 padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
-                      child: Container(
-                        width: 260,
-                        child: Text(snapshot.data[index].titreOffre,style:
-                        TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ), overflow: TextOverflow.ellipsis
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                      child: Container(
-                        decoration: BoxDecoration(
-                        ),
-                        child: Text(snapshot.data[index].typeContrat,textAlign: TextAlign.center,),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                      child: Container(
-                        width: 260,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              new Text(snapshot.data[index].communeEmploi),
-                            ]
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
-                      child: Container(
-                        width: 260,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              new Text(snapshot.data[index].aPourvoirLe),
-                            ]
+                    Container(
+
+                      child:Column(children: <Widget>[
+                        SizedBox(
+                          child: Container(
+                            width: 260,
+                            child: Text(snapshot.data[index].titreOffre,style:
+                            TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ), overflow: TextOverflow.ellipsis
+                            ),
                           ),
                         ),
-                      ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                          child: Container(
+                            decoration: BoxDecoration(
+                            ),
+                            child: Text(snapshot.data[index].typeContrat,textAlign: TextAlign.center,),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                          child: Container(
+                            width: 260,
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  new Text(snapshot.data[index].communeEmploi),
+                                ]
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                          child: Container(
+                            width: 260,
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  new Text(snapshot.data[index].aPourvoirLe),
+                                ]
+                            ),
+                          ),
+                        ),
+                      ]),
+                    )
+
                   ],
                 ),
               ),
               SizedBox(
-                height: height/2,
+
                 child:
                      Image.memory(
-
                         snapshot.data[index].decodeLogo(),
                         fit: BoxFit.contain,
                         color: const Color.fromRGBO(255, 255, 255, 0.5),
